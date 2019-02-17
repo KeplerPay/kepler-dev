1d6644c7342cc2a4a37e896e0bd56c40d0033293
endlessloop2 <mateo@endlessloop.me>
2019-02-17 18:50:46 -0300

    Change version and some network parameters

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -318,9 +318,9 @@ public:
         consensus.nGovernanceMinQuorum = 1;
         consensus.nGovernanceFilterElements = 500;
         consensus.nMasternodeMinimumConfirmations = 1;
-        consensus.BIP34Height = 76;
-        consensus.BIP65Height = 2431; // 0000039cf01242c7f921dcb4806a5994bc003b48c1973ae0c89b67809c2bb2ab
-        consensus.BIP66Height = 2075; // 0000002acdd29a14583540cb72e1c5cc83783560e38fa7081495d474fe1671f7
+        consensus.BIP34Height = 5;
+        consensus.BIP65Height = 10;
+        consensus.BIP66Height = 15; 
         consensus.DIP0001Height = 5500;
         consensus.powLimit[ALGO_SLOT1] = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.powLimit[ALGO_SLOT2] = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
@@ -454,7 +454,7 @@ public:
 
         chainTxData = ChainTxData{        
             1529294335, // * UNIX timestamp of last known number of transactions
-            5810120,    // * total number of transactions between genesis and that timestamp
+            0,    // * total number of transactions between genesis and that timestamp
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0.01        // * estimated number of transactions per second after that timestamp
         };
