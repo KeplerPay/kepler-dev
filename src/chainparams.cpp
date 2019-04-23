fa79af5f6cc54cada42045886dab6b3645a511e8
endlessloop2 <mateo@endlessloop.me>
2019-04-22 22:35:43 -0400

    Add untracked files, build fixes, reset network for rfv2 test

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -80,7 +80,7 @@ static CBlock CreateDevNetGenesisBlock(const uint256 &prevBlockHash, const std::
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "XX/03/2019 noticia";
+    const char* pszTimestamp = "22/04/2019 prueba rfv2";
     const CScript genesisOutputScript = CScript() << ParseHex("04b8bbf7e36419f96fc99b7d9d04a62e8d9a28f6c8dc548e7b9b84b44c380693b76e730f28d18894bc05a0a72d5bb8e35221dc0d375f8552c9485995f60a94d23a") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
@@ -148,7 +148,7 @@ public:
 
         consensus.powLimit[ALGO_SLOT1] = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // NeoScrypt
         consensus.powLimit[ALGO_SLOT2] = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // Argon2-4096
-        consensus.powLimit[ALGO_SLOT3] = uint256S("000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // Rainforest
+        consensus.powLimit[ALGO_SLOT3] = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // Rainforest-v2
         consensus.nPowTargetTimespan = 24 * 60 * 60; // Kepler: 1 day
         
         consensus.nPowTargetSpacing = 120; // 120 second block time, target is set to 120 * NUM_ALGOS, so every algorithm finds a block every 2 mins on avg
@@ -168,10 +168,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000102d00"); // 1 getblockchaininfo: chainwork
+        //consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000102d00"); // 1 getblockchaininfo: chainwork
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x2fc4ae8e56ac19d9dc3f4aba22062d63627b8ea4d918a3d814c763923fa2b836"); // 1
+        //consensus.defaultAssumeValid = uint256S("0x2fc4ae8e56ac19d9dc3f4aba22062d63627b8ea4d918a3d814c763923fa2b836"); // 1
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -187,7 +187,7 @@ public:
         nPruneAfterHeight = 100000;
 
         // timestamp, nNonce, nBits, nVersion, genesisReward 
-        genesis = CreateGenesisBlock(1551144879, 35617760, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1555986291, 36257733, 0x1e0ffff0, 1, 50 * COIN);
         if(false)
         {
             printf("Searching for mainnet genesis block...\n");
@@ -225,8 +225,8 @@ public:
 
 
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0xd3e0a2fffc176160b542a5aab21818c84ed3c30e3cc95d05581c1ebdc8ce74ca"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4e54133d52b64547cf7a0212227a5d3f5e3e1dae207e6793a5519758ce3ae94b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x39b4e86b511344fabd0c1666d1976d4bc938fd08a36c7de47e3f897981a97d16"));
+        assert(genesis.hashMerkleRoot == uint256S("0x39bb0e46780b4afd46c0a9661318ce6575eeee04abc332eac39f563a55fedeb4"));
 
 
         vSeeds.push_back(CDNSSeedData("kepler.cash", "seed1.kepler.cash"));
@@ -261,13 +261,13 @@ public:
         strSporkAddress = "KJqpyN1nKWC9yFMTC3CndTPNJQrRmr8fwK";
 
         checkpointData = (CCheckpointData) {
-            boost::assign::map_list_of
-            (  0, uint256S("0xd3e0a2fffc176160b542a5aab21818c84ed3c30e3cc95d05581c1ebdc8ce74ca"))
-            (  1, uint256S("0x2fc4ae8e56ac19d9dc3f4aba22062d63627b8ea4d918a3d814c763923fa2b836"))
+            //boost::assign::map_list_of
+            //(  0, uint256S("0xd3e0a2fffc176160b542a5aab21818c84ed3c30e3cc95d05581c1ebdc8ce74ca"))
+            //(  1, uint256S("0x2fc4ae8e56ac19d9dc3f4aba22062d63627b8ea4d918a3d814c763923fa2b836"))
         };
 
         chainTxData = ChainTxData{
-            1551234915, // * UNIX timestamp of last known number of transactions
+            1555986291, // * UNIX timestamp of last known number of transactions
             0,    // * total number of transactions between genesis and that timestamp
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0.1         // * estimated number of transactions per second after that timestamp
